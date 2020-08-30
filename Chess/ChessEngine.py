bbd9e6e3debc68edc02c32bd417ec31fc87c10cc
krishx02 <kcg75@scarletmail.rutgers.edu>
2020-08-30 15:46:41 -0400

    Updated what the ChessEngine.py and ChessMain.py are going to be used for in the future. Creating a class and constructor for ChessEngine.py

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -1,3 +1,6 @@
 """"
-This is the driver file. Responsible for handling user input and displaying current GameState.
+Class is responsible for storing all of the information about the current state of the chess game.
+Also be responsible for determining the valid moves at the current state. Also keep a move log.
 """
+class GameState():
+    def __init__(self):
---
+++ Chess/ChessMain.py
@@ -0,0 +1,3 @@
+""""
+This is the driver file. Responsible for handling user input and displaying current GameState object.
+"""
