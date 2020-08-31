ef8ece605e57ef5b58a037fb5e191cbaa5b27da7
krishx02 <kcg75@scarletmail.rutgers.edu>
2020-08-31 16:51:07 -0400

    dumb bug

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -30,7 +30,7 @@ class Move():
     # maps key to values
     # key : value
     ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
-                   "5": 4, "6": 3, "7": 1, "8": 0}
+                   "5": 3, "6": 2, "7": 1, "8": 0}
     rowsToRanks = {v: k for k, v in ranksToRows.items()} #reverses the dictionary
     filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3,
                    "e": 4, "f": 5, "g": 6, "h": 7}
